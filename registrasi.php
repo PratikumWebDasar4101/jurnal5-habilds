e9f88da94062cfc8f7f72a262d5baa4407972b57
Andi Habil Dwi Syaputra <hbildsdsds@gmail.com>
2018-10-01 10:20:59 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Form Registrasi</title>
+	<meta charset="UTF-8">
+	<title>Document</title>
 </head>
 <body>
-
-
-
+	<form action="proses.php" method="post">
+		<table>
+			<tr>
+				<td>Nim</td>
+			</tr>
+			<tr>
+				<td><input type="text" name="nim"></td>
+			</tr>
+			<tr>
+				<td>Nama</td>
+			</tr>
+			<tr>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+			</tr>
+			<tr>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td><input type="submit"></td>
+			</tr>
+		</table>
+	</form>
 </body>
 </html>
