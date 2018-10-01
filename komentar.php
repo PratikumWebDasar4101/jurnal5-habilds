94990b2c35938aa7cf57264987c5c1e77efa1f43
Andi Habil Dwi Syaputra <hbildsdsds@gmail.com>
2018-10-01 10:19:16 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,24 @@
-<?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Komentar</title>
+</head>
+<body>
+	<form action="hasil.php" method="post">
+		<table>
+			<tr>
+				<td>Masukkan Komentar</td>
+			</tr>
+			<tr>
+				<td>
+					<textarea name="komentar" cols="30" rows="10"></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td><input type="submit"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
